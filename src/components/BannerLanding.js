e6319f7ec2d0cc0d302797d7e3b0b30200dd144d
Stackbit Code Editor <projects@stackbit.com>
2021-08-08 14:16:57 +0000

    BannerLanding.js: changed

--- src/components/BannerLanding.js
+++ src/components/BannerLanding.js
@@ -4,10 +4,10 @@ const BannerLanding = (props) => (
     <section id="banner" className="style2">
         <div className="inner">
             <header className="major">
-                <h1>Investire è semplice ma non è facile</h1>
+                <h2>Investire è semplice ma non è facile</h2>
             </header>
             <div className="content">
-                <h2>Warren Buffet</h2>
+                <h3>Warren Buffet</h3>
             </div>
         </div>
     </section>
