b2d2ec748aa1fc1b27f6e9f724de2ed25eabd3fa
Stackbit Code Editor <projects@stackbit.com>
2021-08-08 14:53:04 +0000

    Contact.js: changed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -28,7 +28,7 @@ const Contact = (props) => (
                     <div className="contact-method">
                         <span className="icon alt fa-envelope"></span>
                         <h3>Indirizzo Email</h3>
-                        <a href="#">information@untitled.tld</a>
+                        <a href="mailto:david.passeri@azimut.it" target="_blank" rel="noopener">david.passeri@azimut.it</a>
                     </div>
                 </section>
                 <section>
