e967809663a2cd4576bcc0c03792493a41f63d99
Stackbit Code Editor <projects@stackbit.com>
2021-08-08 14:40:57 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = (props) => (
     <footer id="footer">
         <div className="inner">
             <ul className="icons">
-                <li><a href="#" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
+                <li><a href="#" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a> David</li>
             </ul>
             <ul className="copyright">
                 <li>&copy; David Passeri</li><li><a href="https://www.azimut.it">Consulente Finanziario Azimut</a></li>
