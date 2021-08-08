804c7c88331eaf9d98eb0ea65a6bf8b2cc049d0c
Stackbit Code Editor <projects@stackbit.com>
2021-08-08 14:15:59 +0000

    Banner.js: changed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,7 +7,7 @@ const Banner = (props) => (
                 <h2>"Il rischio nasce dal non sapere cosa stai facendo"</h2>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
+                <h3>A responsive site template designed by HTML5 UP<br />
                 and released under the Creative Commons.</p>
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Get Started</a></li>
