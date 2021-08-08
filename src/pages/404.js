df3242abdaba104a3d4a17b4632b7924b7403fbe
Stackbit Code Editor <projects@stackbit.com>
2021-08-08 16:25:24 +0000

    404.js: changed

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ const NotFoundPage = () => (
         <section id="one">
             <div className="inner">
                 <h1>NOT FOUND</h1>
-                <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+                <p>Hai percorso una strada che non porta a nulla... the sadness.</p>
             </div>
         </section>
     </div>
