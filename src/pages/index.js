313558be9d23051092934e2fb962b05d18a663e7
Stackbit Code Editor <projects@stackbit.com>
2021-08-09 07:37:44 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ class HomeIndex extends React.Component {
                     title="David Passeri - Consulente Finanziario"
                     meta={[
                         { name: 'description', content: 'David Passeri Consulente Finanziario Azimut Capital Management S.p.A..' },
-                        { name: 'keywords', content: 'sample, something' },
+                        { name: 'keywords', content: 'Consulente, Finanziario,' },
                     ]}
                 >
                 </Helmet>
