990ef1b405aa26cab2ed12b212aef47a9274e811
Stackbit Code Editor <projects@stackbit.com>
2021-08-09 07:34:23 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ class HomeIndex extends React.Component {
         return (
             <Layout>
                 <Helmet
-                    title="Gatsby Starter - Forty"
+                    title="David Passeri - Consulente Finanziario"
                     meta={[
                         { name: 'description', content: 'Sample' },
                         { name: 'keywords', content: 'sample, something' },
