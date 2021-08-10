1fc0d1602ecb2391a99fe42fcfcc69fbeb95a55e
Stackbit Code Editor <projects@stackbit.com>
2021-08-10 07:55:46 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -79,7 +79,7 @@ class HomeIndex extends React.Component {
                             </header>
                             <p>Tramite il form sottostante è possibile iscriversi alla newsletter gratuita personalmente creata e gestita. Una volta iscritto, riceverà su base generalmente settimanale: degli articoli sugli ultimi trends in tema economico finanziario, l’analisi tecnica di nuovi strumenti finanziari, delle raccomandazioni agli investitori sulla base dei nuovi scenari macroeconomici. Se desidera contattarmi La invito a fare riferimento alla sezione contatti.</p>
                             <ul className="actions">
-                                <li><Link to="/landing" className="button next">Get Started</Link></li>
+                                <li><Link to="/landing" className="button">Get Started</Link></li>
                             </ul>
                         </div>
                     </section>
