653424354c607d3471fd45142db8933590984893
Stackbit Code Editor <projects@stackbit.com>
2021-08-10 13:07:51 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = (props) => (
     <footer id="footer">
         <div className="inner">
             <ul className="copyright">
-                <li>&copy; David Passeri</li><li>P.I. 0</li><li>Financial Partner</li><li><a href="https://www.azimut.it">Azimut Capital Management SGR S.p.A.</a></li>
+                <li>&copy; David Passeri</li><li>P.I. 03512710546</li><li>Financial Partner</li><li><a href="https://www.azimut.it">Azimut Capital Management SGR S.p.A.</a></li>
             </ul>
         </div>
     </footer>
